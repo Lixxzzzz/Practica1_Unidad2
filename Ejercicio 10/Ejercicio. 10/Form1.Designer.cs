88ac95dcae977fbdae9554a7eac60cf12c31d853
Lizz <yinnarivera150@gmail.com>
2025-06-06 18:27:55 -0400

    Commit

---
+++ Ejercicio 10/Ejercicio. 10/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Ejercicio._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMensaje = new Button();
+            SuspendLayout();
+            // 
+            // btnMensaje
+            // 
+            btnMensaje.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMensaje.Location = new Point(135, 192);
+            btnMensaje.Name = "btnMensaje";
+            btnMensaje.Size = new Size(254, 34);
+            btnMensaje.TabIndex = 0;
+            btnMensaje.Text = "Toca aqui";
+            btnMensaje.UseVisualStyleBackColor = true;
+            btnMensaje.Click += btnMensaje_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Lime;
+            ClientSize = new Size(528, 450);
+            Controls.Add(btnMensaje);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMensaje;
+    }
+}
---
+++ Ejercicio 10/Ejercicio. 10/Form1.cs
@@ -0,0 +1,30 @@
+namespace Ejercicio._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnmensaje_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("¡Hola! SOY YINNA LIZBETH RIVERA ESTRELLA Y (LR-2024-01424) ",
+                            "Información",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void btnMensaje_Click_1(object sender, EventArgs e)
+   
+        {
+
+            MessageBox.Show("¡Hola! SOY YINNA LIZBETH RIVERA ESTRELLA Y (LR-2024-01424) ",
+                            "Información",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+   
+}
+
